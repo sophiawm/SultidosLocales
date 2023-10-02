80b38635472381d40c85cfefa4a1679cc1bf36fb
Sophia WM <claudiasophiawong@gmail.com>
2023-10-02 02:35:58 +0200

    start: base projectsetup

---
+++ database/db.js
@@ -0,0 +1,11 @@
+import { Sequelize } from 'sequelize';
+
+const db =  new Sequelize ('books_app', 'root', 'wonG1996.03', 
+{
+    host:'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    define:{timestamps: false}
+});
+
+export default db;
